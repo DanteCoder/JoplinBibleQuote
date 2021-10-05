880e7475ad827e10bc90c7bb6b3ae836e52b4b77
DanteCoder <90792603+DanteCoder@users.noreply.github.com>
2021-10-05 11:02:36 -0500

    First working attemp

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import joplin from 'api';
+import { ContentScriptType } from 'api/types';
 
 joplin.plugins.register({
 	onStart: async function() {
-		console.info('Hello world. Test plugin started!');
+		await joplin.contentScripts.register(
+			ContentScriptType.MarkdownItPlugin,
+			'bible-quote',
+			'./markdownItPlugin.js'
+		);
 	},
 });
---
+++ src/markdownItPlugin.ts
@@ -0,0 +1,29 @@
+
+export default function() { 
+	return {
+		plugin: function(markdownIt, _options) {
+			const defaultRender = markdownIt.renderer.rules.fence || function(tokens, idx, options, env, self) {
+				return self.renderToken(tokens, idx, options, env, self);
+			};
+		
+			markdownIt.renderer.rules.fence = function(tokens, idx, options, env, self) {
+				const token = tokens[idx];
+				if (token.info !== 'bible') return defaultRender(tokens, idx, options, env, self);
+
+				let html = '';
+
+				let matchs = token.content.match(/\(.*?\)/g)
+
+				if (matchs){
+					for (let match of matchs){
+						html += '<p>';
+						html += match;
+						html += '</p>';
+					};
+				};
+
+				return html;
+			};
+		},
+	}
+}
