58c1e40f52db6e981152b298d00e18a66d5bbb6a
DanteCoder <90792603+DanteCoder@users.noreply.github.com>
2021-10-05 16:30:25 -0500

    Implemented book recognition

---
+++ src/bibles/bible_index.js
@@ -0,0 +1,337 @@
+const bibleIndex = {
+    books: [
+        {
+            name:"genesis",
+            abrv:"gn",
+            num:1
+        },
+        {
+            name:"exodo",
+            abrv:"ex",
+            num:2
+        },
+        {
+            name:"levitico",
+            abrv:"lv",
+            num:3
+        },
+        {
+            name:"numeros",
+            abrv:"nm",
+            num:4
+        },
+        {
+            name:"deuteronomio",
+            abrv:"dt",
+            num:5
+        },
+        {
+            name:"josue",
+            abrv:"jos",
+            num:6
+        },
+        {
+            name:"jueces",
+            abrv:"jue",
+            num:7
+        },
+        {
+            name:"rut",
+            abrv:"rt",
+            num:8
+        },
+        {
+            name:"1 samuel",
+            abrv:"1 s",
+            num:9
+        },
+        {
+            name:"2 samuel",
+            abrv:"2 s",
+            num:10
+        },
+        {
+            name:"1 reyes",
+            abrv:"1 r",
+            num:11
+        },
+        {
+            name:"2 reyes",
+            abrv:"2 r",
+            num:12
+        },
+        {
+            name:"1 cronicas",
+            abrv:"1 cr",
+            num:13
+        },
+        {
+            name:"2 cronicas",
+            abrv:"2 cr",
+            num:14
+        },
+        {
+            name:"esdras",
+            abrv:"esd",
+            num:15
+        },
+        {
+            name:"nehemias",
+            abrv:"neh",
+            num:16
+        },
+        {
+            name:"ester",
+            abrv:"est",
+            num:17
+        },
+        {
+            name:"job",
+            abrv:"job",
+            num:18
+        },
+        {
+            name:"salmos",
+            abrv:"sal",
+            num:19
+        },
+        {
+            name:"proverbios",
+            abrv:"pr",
+            num:20
+        },
+        {
+            name:"eclesiastes",
+            abrv:"ec",
+            num:21
+        },
+        {
+            name:"cantares",
+            abrv:"cnt",
+            num:22
+        },
+        {
+            name:"isaias",
+            abrv:"is",
+            num:23
+        },
+        {
+            name:"jeremias",
+            abrv:"jer",
+            num:24
+        },
+        {
+            name:"lamentaciones",
+            abrv:"lm",
+            num:25
+        },
+        {
+            name:"ezequiel",
+            abrv:"ez",
+            num:26
+        },
+        {
+            name:"daniel",
+            abrv:"dn",
+            num:27
+        },
+        {
+            name:"oseas",
+            abrv:"os",
+            num:28
+        },
+        {
+            name:"joel",
+            abrv:"jl",
+            num:29
+        },
+        {
+            name:"amos",
+            abrv:"am",
+            num:30
+        },
+        {
+            name:"abdias",
+            abrv:"abd",
+            num:31
+        },
+        {
+            name:"jonas",
+            abrv:"jon",
+            num:32
+        },
+        {
+            name:"miqueas",
+            abrv:"miq",
+            num:33
+        },
+        {
+            name:"nahum",
+            abrv:"nah",
+            num:34
+        },
+        {
+            name:"habacuc",
+            abrv:"hab",
+            num:35
+        },
+        {
+            name:"sofonias",
+            abrv:"sof",
+            num:36
+        },
+        {
+            name:"hageo",
+            abrv:"hg",
+            num:37
+        },
+        {
+            name:"zacarias",
+            abrv:"zac",
+            num:38
+        },
+        {
+            name:"malaquias",
+            abrv:"mal",
+            num:39
+        },
+        {
+            name:"mateo",
+            abrv:"mt",
+            num:40
+        },
+        {
+            name:"marcos",
+            abrv:"mc",
+            num:41
+        },
+        {
+            name:"lucas",
+            abrv:"lc",
+            num:42
+        },
+        {
+            name:"juan",
+            abrv:"jn",
+            num:43
+        },
+        {
+            name:"hechos",
+            abrv:"hch",
+            num:44
+        },
+        {
+            name:"romanos",
+            abrv:"ro",
+            num:45
+        },
+        {
+            name:"1 corintios",
+            abrv:"1 co",
+            num:46
+        },
+        {
+            name:"2 corintios",
+            abrv:"2 co",
+            num:47
+        },
+        {
+            name:"galatas",
+            abrv:"gl",
+            num:48
+        },
+        {
+            name:"efesios",
+            abrv:"ef",
+            num:49
+        },
+        {
+            name:"filipenses",
+            abrv:"flp",
+            num:50
+        },
+        {
+            name:"colosenses",
+            abrv:"col",
+            num:51
+        },
+        {
+            name:"1 tesalonicenses",
+            abrv:"1 ts",
+            num:52
+        },
+        {
+            name:"2 tesalonicenses",
+            abrv:"2 ts",
+            num:53
+        },
+        {
+            name:"1 timoteo",
+            abrv:"1 ti",
+            num:54
+        },
+        {
+            name:"2 timoteo",
+            abrv:"2 ti",
+            num:55
+        },
+        {
+            name:"tito",
+            abrv:"tit",
+            num:56
+        },
+        {
+            name:"filemon",
+            abrv:"flm",
+            num:57
+        },
+        {
+            name:"hebreos",
+            abrv:"heb",
+            num:58
+        },
+        {
+            name:"santiago",
+            abrv:"stg",
+            num:59
+        },
+        {
+            name:"1 pedro",
+            abrv:"1 p",
+            num:60
+        },
+        {
+            name:"2 pedro",
+            abrv:"2 p",
+            num:61
+        },
+        {
+            name:"1 juan",
+            abrv:"1 jn",
+            num:62
+        },
+        {
+            name:"2 juan",
+            abrv:"2 jn",
+            num:63
+        },
+        {
+            name:"3 juan",
+            abrv:"3 jn",
+            num:64
+        },
+        {
+            name:"judas",
+            abrv:"jud",
+            num:65
+        },
+        {
+            name:"apocalipsis",
+            abrv:"ap",
+            num:66
+        }
+
+    ]
+}
+
+export default bibleIndex;
--- src/markdownItPlugin.js
+++ src/markdownItPlugin.js
@@ -1,3 +1,5 @@
+const bibleIndex = require('./bibles/bible_index').default;
+
 export default function() { 
 	return {
 		plugin: function(markdownIt, _options) {
@@ -11,18 +13,37 @@ export default function() {
 
 				let html = '';
 
-				let matchs  = token.content.match(/\(.*?\)/g)
+				let quotes  = token.content.match(/\(.*?\)/g)
 
-				if (matchs){
-					for (let match of matchs){
+				if (quotes){
+					for (let quote of quotes){
 
-						match = match.slice(1,-1);
-						let args = match.split(' ');
+						quote = quote.slice(1,-1);
+						let parts = quote.split(' ');
 
-						for (let arg of args){
-							html += '<p>';
-							html += arg;
+						if (parts.length === 3){
+							parts = [parts[0] + ' ' + parts[1], parts[2]];
+						}
+
+						//To remove accents and diacritics
+						const bk = parts[0].normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+						const verse = parts[1];
+
+						let is_a_book = false;
+						let book_num = 0;
+
+						for (let book of bibleIndex.books) {
+							if (bk == book.name || bk == book.abrv){
+								is_a_book = true;
+								book_num = book.num;
+							}
+						}
+						
+						if (is_a_book){
+							html += '<p>Indeed it is a book, whom number is: ';
+							html += book_num.toLocaleString();
 							html += '</p>';
+
 						}
 
 					};
