5e32f2c41d30701a15aba751684470145d9390d5
Dante Barboza <danteg.barboza@gmail.com>
2022-02-21 11:44:49 -0600

    Improvement to xmlBible2Js error handling

--- src/utils/xmlBible2Js.ts
+++ src/utils/xmlBible2Js.ts
@@ -4,25 +4,24 @@ import { parseString as parseXmlString } from 'xml2js';
 /**
  * Parses an xml bible to a JS object
  * @param biblePath The path to the xml file
- * @returns Parsed xml bible or error
+ * @returns Parsed xml bible and error
  */
-export function xmlBible2Js(biblePath: string): returnValue {
-  let parsedBible: any;
+export function xmlBible2Js(biblePath: string): ReturnValue {
+  let returnValue: ReturnValue;
   let xmlFile: any;
   try {
     xmlFile = fs.readFileSync(biblePath, 'utf8');
   } catch (error) {
-    return { parsedBible: null, error: error.message };
+    return { parsedBible: null, error };
   }
-  parseXmlString(xmlFile, (err, result) => {
-    if (err) return { parsedBible: null, error: err.message };
-    parsedBible = result;
+  parseXmlString(xmlFile, (error: Error, result: any) => {
+    returnValue = { parsedBible: result, error };
   });
 
-  return { parsedBible, error: false };
+  return returnValue;
 }
 
-interface returnValue {
+interface ReturnValue {
   parsedBible: any;
-  error: any;
+  error: Error;
 }
