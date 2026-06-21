2ac93c6f700b6167f987c8b40a6fb6ef6d26aaa3
Dante Barboza <90792603+DanteCoder@users.noreply.github.com>
2026-06-20 18:08:12 -0600

    chore: migrate to webviewApi instead of using localstorage

--- src/utils/getPluginConfig.ts
+++
@@ -1,7 +0,0 @@
-import { PluginConfig } from '../interfaces/config';
-
-export function getPluginConfig(): PluginConfig {
-  const localStorageConfig: PluginConfig = JSON.parse(localStorage.getItem('bibleQuotePlugin') ?? '{}');
-
-  return localStorageConfig;
-}
